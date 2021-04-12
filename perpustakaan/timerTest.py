15c34500df4586986b366179dfa02d4ce98d6390
kimimccaw <amirhakimibin.azman@gmail.com>
2021-04-12 08:55:20 +0200

    added comment to make it easier to understand

--- perpustakaan/timerTest.py
+++ perpustakaan/timerTest.py
@@ -8,6 +8,8 @@ import versuchsstand
 from time import strftime, localtime
 from versuchsstand import cerabar, heater, i2c_bus, mcp3008, modbus, promass, pump, sht85, thermophant, trime
 
+#this is personal script by kimi to take measurement from the versuchsstand
+# This script is already customized to send all the data(in .csv format) into a telegram-chat group
 
 def record_data(record_time, time_interval, pump_frequency, heat, remarks):
 
@@ -17,6 +19,7 @@ def record_data(record_time, time_interval, pump_frequency, heat, remarks):
     bus_trime = trime.init_trime()
 
     #initializing versuchsstand actuators
+    #includes pump and heater
     instrument = modbus.init_pump()
     pump.pump_on_off(instrument,True)
     heater.init_heater()
@@ -25,6 +28,7 @@ def record_data(record_time, time_interval, pump_frequency, heat, remarks):
     time.sleep(2)
     pump_freq = pump_frequency
     pump.pump_set_frequency(instrument,pump_freq)
+
     #starting recording procedure
     cwd = os.getcwd()
     masa = strftime("%a, %d %b %Y %H:%M:%S +0000", localtime())
@@ -39,36 +43,32 @@ def record_data(record_time, time_interval, pump_frequency, heat, remarks):
     else:
         notes = remarks
 
-    #path_xlsx = os.path.join(cwd, 'versuchsstand.xlsx')
-
+    #Sending message to terminal to mark the begin of data recording
     print('Data recording begin')
 
+    #Data recording
+    #Include reading the sensor measurements from multiple sensors on the versuchsstand
+    #Automatically store and create the new data in csv file, meanwhile delete the old/previous data
     with open( path_csv , 'w', newline='') as f:    
+
         fieldnames = ['Time', 'Pump_frequency', 'Thermophant_reading', 'Cerabar_reading', 'Promass_reading', 'Ambient_temperature', 'Ambient_humidity', 'Trime_moist', 'Trime_transit_time', 'Trime_pseudo_time', 'Trime_moist_1', 'Trime_transit_time_1', 'Trime_pseudo_time_1' ]
         thewriter = csv.DictWriter(f, fieldnames= fieldnames)
-
 
         thewriter.writeheader()
         for i in range(int(record_time/time_interval)) :
             time.sleep(time_interval)
-            #if pump_freq <= 350 :
-            #    pump_freq +=5
-            #else:
-            #    pump_freq = 100 
-            #print(pump_freq)
             pump.pump_set_frequency(instrument,pump_freq)
+
             try:
                 reading = trime.read_sensor(bus_trime,48101)
             except:
                 reading = trime.read_sensor(bus_trime,48101)
 
-
             try:
                 reading_1 = trime.read_sensor(bus_trime,48012)
             except:
                 reading_1 = trime.read_sensor(bus_trime,48012)
-            
-            
+                        
             thewriter.writerow({'Time': strftime("%a, %d %b %Y %H:%M:%S +0000", localtime()),'Pump_frequency': pump_freq ,
                                 'Thermophant_reading' : thermophant.read_value(spi), 'Cerabar_reading' : cerabar.read_value(spi),
                                 'Promass_reading': promass.read_value(spi) ,'Ambient_temperature':sht85.read_ambient_values(bus_i2c)[0], 'Ambient_humidity':sht85.read_ambient_values(bus_i2c)[1],
@@ -76,22 +76,20 @@ def record_data(record_time, time_interval, pump_frequency, heat, remarks):
             
 
     #turning off the actuator
+    #Turning off the pump slowly
     time.sleep(3)
     pump.pump_set_frequency(instrument,30)
     time.sleep(3)
     pump.pump_set_frequency(instrument,0)
     time.sleep(3)
     pump.pump_on_off(instrument,False)
-    
+    #Turning off the heater if it's on in the first place
     if heat is True:
         heater.heater_on_off(False)  
     
-    #df = pd.read_csv(path_csv)
-    #writer = pd.ExcelWriter(path_xlsx)
-    #df.to_excel(writer, index = False)
-    #writer.save()
 
-
+    #Saving the data in a csv file format
+    #Sending the data to a telegram-chat app by using a bot
     chat_id = '-441623220'
     files = {'document' : open(path_csv, 'rb')}
     base_Document = 'https://api.telegram.org/bot1434204376:AAH275V5dKSTfiVDzzKE122T05t7f7Cosc0/sendDocument?chat_id={}&caption="{}"'.format(chat_id,(masa + ' remarks: ' + notes))
@@ -101,7 +99,5 @@ def record_data(record_time, time_interval, pump_frequency, heat, remarks):
 
     print('Operation complete')
 
-
-#record_data(60,5)
 
 
--- perpustakaan/versuchsstand/trime.py
+++ perpustakaan/versuchsstand/trime.py
@@ -1,13 +1,13 @@
-## This module is for connection to the trime bus protocol
+## This module is for connection to the trime bus protocol to the trime sensor
 import implib2
 
-
+#Initialization of the trime bus connection to the designated port
 def init_trime():
     bus_trime = implib2.Bus('/dev/ttyUSB_VARIANT')
     bus_trime.sync()
     return bus_trime
 
-#serial_no; 48101: SondeVariant1 , 48102: WickelSonde
+#serial_no; 48101: SondeVariant1 , 48012: WickelSonde
 def read_sensor(bus_trime, serial_no):
     module = implib2.Module(bus_trime, serial_no)
     module.set_event_mode()
